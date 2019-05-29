db213d803566113fa8882cf8fd924aac5183f6a1
Dako Mumladze <dmumladze@corp.europe-bet.com>
2019-05-29 16:18:23 +0400

    implementing recipe model and controller

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = "https://cors-anywhere.herokuapp.com/";
+export const key = 'de3bce6f53e1dae16792b476868b18af';
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,5 @@
-// de3bce6f53e1dae16792b476868b18af 
-// https://www.food2fork.com/api/search
-
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import { elements,renderLoader,clearLoader } from './views/base';
 import * as searchView from './views/searchView';
 //Global state of the App
@@ -22,11 +20,17 @@ const controlSearch = async () =>{
         searchView.clearInput();
         searchView.clearResults();
         renderLoader(elements.searchRes);
-        //4.Search for recipes
-        await state.search.getResults();
-        //5.Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+        try{
+            //4.Search for recipes
+            await state.search.getResults();
+            //5.Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        }catch(err){
+            alert(err);
+            clearLoader();
+        }
+
     }
 }
 
@@ -42,3 +46,36 @@ elements.searchResPages.addEventListener('click', e => {
         searchView.renderResults(state.search.result,goToPage);
     }
 })
+
+// Recipe Controller
+//Test data results
+// const r = new Recipe(47025);
+// r.getRecipe();
+// console.log(r);
+
+//Getting the id from url
+const controlRecipe = async () => {
+    const id = window.location.hash.replace('#','');
+    console.log(id);
+    if(id){
+        try{
+        //Prepare UI for changes
+
+        //Create new recipe object
+        state.recipe = new Recipe(id);
+        //Get Recipe data
+        await state.recipe.getRecipe();
+        //Calculate servings and time
+        state.recipe.calcTime();
+        state.recipe.calcServings();
+        //Render the recipe
+        console.log(state.recipe);
+        }catch(err){
+            alert(err);
+        }
+
+
+    }
+}
+
+['hashchange','load'].forEach(event => window.addEventListener(event,controlRecipe));
